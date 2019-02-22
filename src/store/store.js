dccf101c6bdcdbfb0e6b90dc071415ddd173ba32
Jose M. Vasconcellos <miguel_vasconce@hotmail.com>
2019-02-22 17:37:08 -0500

    Added BlackList Filter for login reducer on redux-persist

--- src/store/store.js
+++ src/store/store.js
@@ -3,10 +3,21 @@ import thunk from 'redux-thunk';
 import rootReducer from '../Redux/reducers/index';
 import storage from 'redux-persist/lib/storage';
 import { persistStore, persistReducer } from 'redux-persist';
+import { createBlacklistFilter } from 'redux-persist-transform-filter';
 
 let middleware = [thunk];
 
-let config = { key: 'root', storage }
+let saveSubsetBlacklistFilter = createBlacklistFilter(
+  'form',
+  ['login']
+);
+
+let config = {
+  key: 'root',
+  storage: storage,
+  //blacklist: ['form'],
+  transforms: [saveSubsetBlacklistFilter]
+}
 
 let store = createStore(persistReducer(config, rootReducer), compose(applyMiddleware(...middleware), window.__REDUX_DEVTOOLS_EXTENSION__&& window.__REDUX_DEVTOOLS_EXTENSION__()));
 
