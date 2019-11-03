29c0f979bb10c62e5778ae0c92c8b1b186c00d26
hAvOc-lap <hhavocs@yahoo.com>
2019-11-03 08:48:15 -0500

    #21 Internal Payroll

---
+++ src/Pages/InternalPayroll/NewInternalPayroll/NewInternalPayroll.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+import { push } from 'connected-react-router';
+import PropTypes from 'prop-types';
+import { Translate, withLocalize } from 'react-localize-redux';
+import { connect } from 'react-redux';
+import ActiveLanguageAddTranslation from '../../../components/common/ActiveLanguageAddTranslation/ActiveLanguageAddTranslation.js';
+import { tempedgeAPI } from '../../../Redux/actions/tempEdgeActions';
+
+
+class NewInternalPayroll extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            
+        }
+
+        ActiveLanguageAddTranslation(this.props.activeLanguage, this.props.addTranslationForLanguage);
+    }
+
+    componentDidUpdate(prevProps, prevState) {
+        const hasActiveLanguageChanged = prevProps.activeLanguage !== this.props.activeLanguage;
+
+        if (hasActiveLanguageChanged) {
+            this.props.push(`/intpayroll/new/${this.props.activeLanguage.code}`);
+            ActiveLanguageAddTranslation(this.props.activeLanguage, this.props.addTranslationForLanguage);
+        }
+    }
+
+    render() {
+
+        return (
+            <div>test</div>
+        )
+    }
+}
+
+NewInternalPayroll.propTypes = {     //Typechecking With PropTypes, will run on its own, no need to do anything else, separate library since React 16, wasn't the case before on 14 or 15
+   //Action, does the Fetch part from the posts API
+   tempedgeAPI: PropTypes.func.isRequired,     //Action, does the Fetch part from the posts API
+}
+
+export default withLocalize(connect(null, { push, tempedgeAPI })(NewInternalPayroll));
