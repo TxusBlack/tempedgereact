1a60ad2d7f3d957ea8f22d362afd5c5067709d75
Felipe Andrés López <felipeandreslopez91@gmail.com>
2020-02-06 17:18:44 -0500

    Se finaliza el desarrollo de las tareas #40 y #41, también se corrgieron unos bugs de las tareas #34 y #36. Queda pendiente terminar las pruebas

--- src/components/common/ToggleSwitch/ToggleSwitch.js
+++ src/components/common/ToggleSwitch/ToggleSwitch.js
@@ -1,24 +1,18 @@
-import React from "react";
-import "../../../assets/styles/components/ToggleSwitch.css";
+import React from 'react';
+import '../../../assets/styles/components/ToggleSwitch.css';
 
-class ToggleSwitch extends React.Component {
-  handleChange(e) {
-    //this.props.onClick(e.target.checked);
-  }
-
-  render() {
-    return (
-      <div className="toggle-switch">
-        <input
-          onChange={e => this.handleChange(e)}
-          type="checkbox"
-          className="toggle-switch__checkbox"
-        />
-        <div className="toggle-switch__knobs"></div>
-        <div className="toggle-switch__layer"></div>
-      </div>
-    );
-  }
+const ToggleSwitch = (formProps) => {
+  return (
+    <div className="toggle-switch">
+      <input
+        onChange={e => formProps.input.onChange(e.target.checked)}
+        type="checkbox"
+        className="toggle-switch__checkbox"
+      />
+      <div className="toggle-switch__knobs"></div>
+      <div className="toggle-switch__layer"></div>
+    </div>
+  );
 }
 
 export default ToggleSwitch;
