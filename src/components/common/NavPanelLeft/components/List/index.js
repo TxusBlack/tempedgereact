6e5f6004d0538af2944a8232ab3983d9eba6fd25
Felipe Andrés López <felipeandreslopez91@gmail.com>
2020-04-08 23:11:56 -0500

    #44 Se corrige el menú y submenu

--- src/components/common/NavPanelLeft/components/List/index.js
+++ src/components/common/NavPanelLeft/components/List/index.js
@@ -1,50 +1,47 @@
-import React from "react";
+import React from 'react';
 import { Link } from 'react-router-dom';
-import history from "../../../../../history.js"
-import helpers from "../../../../../utils/helpers";
+import history from '../../../../../history.js';
+import helpers from '../../../../../utils/helpers';
 
-class List extends React.Component{
-  state = {
-    parentBackground: false,
-    subMenu: []
+class List extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      parentBackground: false,
+      subMenu: []
+    };
   }
 
-  displaySubMenu = (index) => {
+  displaySubMenu = (e, index) => {
+    e.target.closest('li').classList.toggle('menu-option-selected');
     let subMenu = this.state.subMenu;
     subMenu[index] = !subMenu[index];
-
     this.setState(() => ({
       subMenu: subMenu
     }));
-  }
+  };
 
   onClickRedirect = (url, index) => {
-    let subMenu = this.state.subMenu;
-    subMenu[index] = false;
+    this.props.onClick();
+    history.push(url);
+  };
 
-    this.setState(() => ({
-      subMenu: subMenu
-    }), () => {
-      this.props.onClick();
-      history.push(url);
-    });
-  }
-
-  render(){
+  render() {
     let { onClick, activeLanguage, list } = this.props;
     let parents = [];
     let children = [];
-    let lista = (Array.isArray(list) && list.length > 0)? list: [];
+    let lista = Array.isArray(list) && list.length > 0 ? list : [];
 
     lista.map((item, index) => {
-      if(item.parent === null){
+      if (item.parent === null) {
         parents.push({
           category: item.optionName,
           optionPath: item.optionPath,
           children: [],
           menuOrder: item.menuOrder
         });
-      }else{
+      } else {
         children.push(item);
       }
     });
@@ -52,54 +49,68 @@ class List extends React.Component{
     children.map((item, index) => {
       let idx = helpers.findByProp(parents, item.parent, 'category');
 
-      if(idx < 0){
+      if (idx < 0) {
         parents.push({
           category: item.parent,
-          optionPath: "",
+          optionPath: '',
           children: [],
           menuOrder: item.menuOrder
         });
       }
     });
 
-    parents.sort((a, b) => (a.menuOrder > b.menuOrder) ? 1 : -1);
-    children.sort((a, b) => (a.menuOrder > b.menuOrder) ? 1 : -1);
+    parents.sort((a, b) => (a.menuOrder > b.menuOrder ? 1 : -1));
+    children.sort((a, b) => (a.menuOrder > b.menuOrder ? 1 : -1));
 
     parents.map((parent, x) => {
       children.map((child, y) => {
-        if(parent.category === child.parent){
+        if (parent.category === child.parent) {
           parent.children.push(child);
         }
       });
     });
 
-
     let menu = [];
 
     parents.map((item, index) => {
-      if(item.children.length < 1){
-        menu.push(<li><Link onClick={onClick} to={`${item.optionPath}/${activeLanguage} `} style={{marginLeft: 0, cursor: "pointer"}}><span>{item.category}</span></Link></li>);
-      }else{
+      if (item.children.length < 1) {
         menu.push(
-          <React.Fragment>
-            <li onClick={() => this.displaySubMenu(item.category)}><Link style={{ cursor: "pointer" }} to="#">{item.category}&nbsp;&nbsp;&nbsp;&nbsp;<i class="fa fa-chevron-down" aria-hidden="true"></i></Link></li>
-            {
-              (this.state.subMenu[item.category])? (
-                <ul style={{ paddingTop: 0 }}>
-                  {item.children.map((child, j) => {
-                    return <li onClick={() => this.onClickRedirect(`${child.optionPath}/${activeLanguage} `, child.parent)} style={{cursor: "pointer"}}><span>{child.optionName}</span></li>
-                  })}
-                </ul>
-              ): ""
-            }
-          </React.Fragment>
+          <Link onClick={onClick} to={`${item.optionPath}/${activeLanguage} `} style={{ marginLeft: 0, cursor: 'pointer' }}>
+            <li>
+              <span>{item.category}</span>
+            </li>
+          </Link>
+        );
+      } else {
+        menu.push(
+          <>
+            <Link onClick={(e) => this.displaySubMenu(e, item.category)} style={{ cursor: 'pointer' }} to="#">
+              <li className="d-flex justify-content-between">
+                {item.category}
+                <span className="down-arrow">﹀</span>
+              </li>
+            </Link>
+            {this.state.subMenu[item.category] ? (
+              <ul className="sub-menu">
+                {item.children.map((child, j) => {
+                  return (
+                    <li onClick={() => this.onClickRedirect(`${child.optionPath}/${activeLanguage} `, child.parent)} style={{ cursor: 'pointer' }}>
+                      <span>{child.optionName}</span>
+                    </li>
+                  );
+                })}
+              </ul>
+            ) : (
+              ''
+            )}
+          </>
         );
       }
     });
 
-    return(
-      <ul style={{}}>
-        { menu }
+    return (
+      <ul className="main-menu" style={{}}>
+        {menu}
       </ul>
     );
   }
