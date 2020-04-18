8178464080f0db9751d80b9fd6d012f8bf027fb7
Diego Moreno <diego.moreno@tempedge.com>
2020-04-18 12:22:18 -0500

    #30 Component for upload file created

--- src/Pages/ApplyNow/index.js
+++ src/Pages/ApplyNow/index.js
@@ -2,6 +2,11 @@ import React, { Component } from 'react';
 import { Translate, withLocalize } from 'react-localize-redux';
 import { connect } from 'react-redux';
 import ActiveLanguageAddTranslation from '../../components/common/ActiveLanguageAddTranslation/ActiveLanguageAddTranslation.js';
+import ProgressBar from 'react-bootstrap/ProgressBar';
+import ContainerBlue from '../../components/common/Container/ContainerBlue.js';
+import PropTypes from 'prop-types';
+import { reduxForm } from 'redux-form';
+import Validate from '../Validations/Validations';
 
 class ApplyNow extends Component {
   constructor(props) {
@@ -21,13 +26,76 @@ class ApplyNow extends Component {
     }
   }
 
+  onSubmit = () => {
+
+  }
+
   render() {
-    return (
-      <div className="container-fluid login-container" style={{ width: '80vw' }}>
-        ApplyNow
-      </div>
-    )
+    // return (
+    //   <div className="container-fluid login-container" style={{ width: '80vw' }}>
+    //     ApplyNow
+    //   </div>
+    // )
+
+    const { btnDisabled, now, resultBar } = this.state;
+    const { handleSubmit } = this.props;
+
+    const bodyContainer = (
+      <form className="panel-body" onSubmit={handleSubmit(this.onSubmit)}>
+        <div className="row">
+          <div className="col-12">{resultBar}</div>
+        </div>
+
+        <div className="form-group row">
+          <div className="col-12">
+            <p className="text-left label-p">
+              <Translate id="com.tempedge.msg.label.uploadEmployeeList" />
+            </p>
+            <div className="input-group">
+              <label htmlFor="employeeListFile" className="input-group-btn">
+                <span className="btn department-list-button">
+                  <Translate id="com.tempedge.msg.label.choosefile" />
+                  <input id="employeeListFile" type="file" onChange={(e) => this.onChange(e)} className="d-none" accept=".xlsx, .xls" />
+                </span>
+              </label>
+              <br />
+              <div className="w-100">
+                <p className="text-left" ref={this.fileNameTextBox} />
+              </div>
+            </div>
+          </div>
+        </div>
+        <ProgressBar animated now={now} label={`${now}%`} />
+        <div className="form-group">
+          <button type="submit" className="btn btn-primary btn-block" disabled={btnDisabled}>
+            <Translate id="com.tempedge.msg.label.upload" />
+          </button>
+        </div>
+      </form>
+    );
+    return <ContainerBlue title="com.tempedge.msg.label.uploadEmployeeList" children={bodyContainer} width="40%" />;
   }
 }
 
-export default withLocalize(connect(null, null)(ApplyNow));
+ApplyNow.propTypes = {
+  reset: PropTypes.func.isRequired,
+  tempedgeAPI: PropTypes.func.isRequired,
+  clearTempedgeStoreProp: PropTypes.func.isRequired,
+  handleSubmit: PropTypes.func.isRequired,
+  notify: PropTypes.func.isRequired
+};
+
+const mapStateToProps = ({ tempEdge }) => ({ saveEmployeeList: tempEdge.saveEmployeeList, countryRegionList: tempEdge.country_region_list });
+
+const applyNow = reduxForm({
+  form: 'applyNow',
+  validate: Validate
+})(ApplyNow);
+
+// export default withLocalize(connect(null, null)(ApplyNow));
+
+export default withLocalize(
+  connect(mapStateToProps, 
+    null
+  )(applyNow)
+);
